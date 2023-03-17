9a604ceeed365d2632dfd01bc2ac7f57af3f0ea6
Ilayda Guzel <ilaydanurguzel1@gmail.com>
2023-03-17 13:48:54 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 
 // Write your code here
+
+//1
+let multiply;
+
+const num1=31;
+const num2=2;
+
+multiply=num1*num2;
+console.log(multiply);
+
+//2
+const random=(Math.random()+1);
+console.log(random);
+
+//3
+let nom;
+
+const num3=19;
+const num4=4;
+
+nom=num4
+nom=(num4)%(num3);
+console.log(nom);
+
+//4
+const set1 = 5;
+const set2 = 15;
+const set3 = 20;
+
+const max=Math.max(set1,set2,set3);
+console.log(max);
+
